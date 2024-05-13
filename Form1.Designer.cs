286bf16e9c38f6c20ad2cb705c7cd0c5fec5af77
nhuzjak20 <BorusiaDortmund15>
2024-05-13 22:51:45 +0200

    Napravljeno pola sučelja

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.UnosStudenta = new System.Windows.Forms.Button();
+            this.BoxUnosStudenta = new System.Windows.Forms.TextBox();
+            this.RegistracijaStudenta = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.RegistracijaProizvoda = new System.Windows.Forms.Button();
+            this.UnosProizvoda = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.KodProizvodaLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // UnosStudenta
+            // 
+            this.UnosStudenta.Location = new System.Drawing.Point(177, 12);
+            this.UnosStudenta.Name = "UnosStudenta";
+            this.UnosStudenta.Size = new System.Drawing.Size(131, 22);
+            this.UnosStudenta.TabIndex = 0;
+            this.UnosStudenta.Text = "Unos Studenta";
+            this.UnosStudenta.UseVisualStyleBackColor = true;
+            // 
+            // BoxUnosStudenta
+            // 
+            this.BoxUnosStudenta.Location = new System.Drawing.Point(12, 12);
+            this.BoxUnosStudenta.Name = "BoxUnosStudenta";
+            this.BoxUnosStudenta.Size = new System.Drawing.Size(150, 22);
+            this.BoxUnosStudenta.TabIndex = 1;
+            this.BoxUnosStudenta.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // RegistracijaStudenta
+            // 
+            this.RegistracijaStudenta.Location = new System.Drawing.Point(432, 293);
+            this.RegistracijaStudenta.Name = "RegistracijaStudenta";
+            this.RegistracijaStudenta.Size = new System.Drawing.Size(195, 28);
+            this.RegistracijaStudenta.TabIndex = 2;
+            this.RegistracijaStudenta.Text = "Registracija Studenta";
+            this.RegistracijaStudenta.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(296, 332);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // RegistracijaProizvoda
+            // 
+            this.RegistracijaProizvoda.Location = new System.Drawing.Point(546, 81);
+            this.RegistracijaProizvoda.Name = "RegistracijaProizvoda";
+            this.RegistracijaProizvoda.Size = new System.Drawing.Size(155, 23);
+            this.RegistracijaProizvoda.TabIndex = 4;
+            this.RegistracijaProizvoda.Text = "Registracija Proizvoda";
+            this.RegistracijaProizvoda.UseVisualStyleBackColor = true;
+            // 
+            // UnosProizvoda
+            // 
+            this.UnosProizvoda.Location = new System.Drawing.Point(546, 52);
+            this.UnosProizvoda.Name = "UnosProizvoda";
+            this.UnosProizvoda.Size = new System.Drawing.Size(161, 23);
+            this.UnosProizvoda.TabIndex = 5;
+            this.UnosProizvoda.Text = "Unos Proizvoda";
+            this.UnosProizvoda.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(415, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // KodProizvodaLabel
+            // 
+            this.KodProizvodaLabel.AutoSize = true;
+            this.KodProizvodaLabel.Location = new System.Drawing.Point(314, 56);
+            this.KodProizvodaLabel.Name = "KodProizvodaLabel";
+            this.KodProizvodaLabel.Size = new System.Drawing.Size(95, 16);
+            this.KodProizvodaLabel.TabIndex = 7;
+            this.KodProizvodaLabel.Text = "Kod Proizvoda";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(713, 381);
+            this.ClientSize = new System.Drawing.Size(713, 399);
+            this.Controls.Add(this.KodProizvodaLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.UnosProizvoda);
+            this.Controls.Add(this.RegistracijaProizvoda);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.RegistracijaStudenta);
+            this.Controls.Add(this.BoxUnosStudenta);
+            this.Controls.Add(this.UnosStudenta);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button UnosStudenta;
+        private System.Windows.Forms.TextBox BoxUnosStudenta;
+        private System.Windows.Forms.Button RegistracijaStudenta;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button RegistracijaProizvoda;
+        private System.Windows.Forms.Button UnosProizvoda;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label KodProizvodaLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace CollegeRestraunt
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
