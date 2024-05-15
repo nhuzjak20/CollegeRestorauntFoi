6821779dd050ca6516909f3b520ea2424efe9b30
nhuzjak20 <BorusiaDortmund15>
2024-05-15 15:32:02 +0200

    Prije Testiranja Baze

--- RegistracijaProizvoda.Designer.cs
+++ RegistracijaProizvoda.Designer.cs
@@ -90,7 +90,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(398, 450);
+            this.ClientSize = new System.Drawing.Size(398, 143);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
--- classes/Class1.cs
+++ classes/Class1.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient
 using System.Threading.Tasks;
 
 namespace CollegeRestraunt.classes
 {
     internal class DBclass
     {
-        string ServerName, Login, Password, Database;
-        public DBclass(string sname, string login, string password, string database)
+        string Connection = "Data Source=31.147.206.65;Initial Catalog=PI2324_dsedlan22_DB;Persist Security Info=True;User ID=PI2324_dsedlan22_User;Password=!;sx].Ga";
+        public DBclass()
         {
-            ServerName = sname;
-            Login = login;  
-            Password = password;
-            Database = database;
+            SqlConnection konekcija = new SqlConnection(Connection);
+            
+            konekcija.Open();
+            
+        }
+
+        public void InsertStudent(string ImeIPrezime, int Jmbag)
+        {
+            SqlConnection konekcija = new SqlConnection(Connection);
+
+            konekcija.Open();
+
         }
     }
 }
